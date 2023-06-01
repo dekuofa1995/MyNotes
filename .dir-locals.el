6986e35aef8679e56ab3304276d59d093683137d
dekuofa1995 <56107353+dekuofa1995@users.noreply.github.com>
2023-06-01 09:57:20 +0800

    config: add to denote subdirs

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables.  For more information evaluate:
+;;;
+;;;     (info "(emacs) Directory Variables")
+;;; from https://protesilaos.com/emacs/denote#h:15719799-a5ff-4e9a-9f10-4ca03ef8f6c5
+((nil . ((denote-directory . default-directory))))
